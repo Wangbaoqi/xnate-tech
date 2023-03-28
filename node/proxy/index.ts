22f2b7fef5a13bfe988bc4f669c6c157f5a2e485
Wangbaoqi <wangbaoqi8839@gmail.com>
2023-03-28 15:19:43 +0800

    feat: learn ts

---
+++ node/proxy/index.ts
@@ -0,0 +1,41 @@
+import fetch from 'node-fetch';
+import { HttpsProxyAgent } from 'https-proxy-agent';
+
+
+const proxy = 'http://127.0.0.1:7890';
+
+const agent = new HttpsProxyAgent(proxy);
+
+
+const request = async() => {
+
+  try {
+     const res = await fetch('https://api.openai.com/v1/chat/completions', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer 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'
+        },
+        body: {
+          "model": "gpt-3.5-turbo",
+          "messages": [
+            {
+              "role": "user",
+              "content": "Say this is a test!"
+            }
+          ],
+          "temperature": 0.7
+        },
+        agent
+     })
+      
+    console.log(res.json(), 'res');
+    
+    
+  } catch (error) {
+    console.log(error, 'error');
+  }
+  
+}
+
+request()
